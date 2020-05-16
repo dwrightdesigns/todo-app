971ca4cf4434a70b5ce2868052130fa89e0aec38
PUT YOUR NAME HEREdwrightdesigns <denise@denisewrightdesigns.com>
2020-05-15 21:38:10 -0500

    starting react project
    rendered sidebar, navigation, and todo list.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// function toggleSidebar() {
+//   document.querySelector(".sidebar").classList.toggle("active");
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="sidebar">
+        <div className="toggle-btn" onlick="toggleSidebar()">
+          <i className="fas fa-bars"></i>
+        </div>
+        <div className="user">
+          <i class="fas fa-user-circle"></i>
+          <h3>Denise Wright</h3>
+        </div>
+        <ul>
+          <li>My Tasks</li>
+          <li>My Profile</li>
+          <li>Settings</li>
+        </ul>
       </header>
-    </div>
+      <main>
+        <div className="logo">
+          <img src="./logo192.png" />
+        </div>
+        <section className="sub__header">
+          <h3>My Tasks</h3>
+          <button>Edit Tasks</button>
+        </section>
+        <section className="inner__frame">
+          <ul>
+            <li>Shopping List</li>
+            <li>School Assignments</li>
+            <li>Vacation Packing List</li>
+          </ul>
+          <div className="new__task">
+            <i class="fas fa-plus-circle"></i>
+            <h3>Create New Task</h3>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <cite>
+          Designed By:{" "}
+          <a href="https://denisewrightdesigns.com">Denise Wright</a>
+        </cite>
+      </footer>
+    </>
   );
 }
 
