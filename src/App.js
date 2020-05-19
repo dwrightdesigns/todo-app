86610565151c2f08d9099816fc2ee7e26ca2d6ae
PUT YOUR NAME HEREdwrightdesigns <denise@denisewrightdesigns.com>
2020-05-19 16:35:43 -0500

    Classes & State
    Completed Classes & State Activity

--- src/App.js
+++ src/App.js
@@ -1,58 +1,74 @@
 import React from "react";
 import "./App.css";
 import Task from "./components/Task";
-import Nav from "./components/Nav";
+// import Nav from "./components/menu/Menu";
 import Header from "./components/Header";
 import Subheader from "./components/subheader";
 import Additem from "./components/Additem";
 import Footer from "./components/footer";
+import { Burger, Menu } from "./components";
 
-const user = {
-  name: "Denise Wright",
-  avatar: "https://bit.ly/3fWa4Gw",
-};
+class App extends React.Component {
+  state = {
+    tasks: [
+      {
+        id: 1,
+        title: "Shopping List",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "School Assignments",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Vacation Packing List",
+        completed: false,
+      },
+    ],
+    user: {
+      name: "Denise Wright",
+      avatar: "https://bit.ly/3fWa4Gw",
+    },
+    menuActive: false,
+  };
 
-const tasks = [
-  {
-    id: 1,
-    title: "Shopping List",
-    completed: false,
-  },
-  {
-    id: 2,
-    title: "School Assignments",
-    completed: false,
-  },
-  {
-    id: 3,
-    title: "Vacation Packing List",
-    completed: false,
-  },
-];
+  toggleMenu = (toggle) => {
+      this.setState({menuActive: toggle})
+  }
 
-function App() {
-  return (
-    <>
-      <Nav avatar={user.avatar} name={user.name} />
-      <Header />
-      <main>
-        <section className="sub__header">
-          <Subheader subheader="My Tasks" button="Edit Tasks" />
-        </section>
-        <section className="list_box">
-          <ul>
-            {tasks.map((taskObj, index) => (
-              <Task task={taskObj} key={index} />
-            ))}
-          </ul>
-          <div className="new__task">
-            <Additem add="Create New Task" />
-          </div>
-        </section>
-      </main>
-          <Footer/>
-    </>
-  );
+  render() {
+
+    return (
+      <>
+        <Burger open={this.state.menuActive} setOpen={this.toggleMenu} />
+        <Menu open={this.state.menuActive} setOpen={this.toggleMenu} avatar={this.state.user.avatar} name={this.state.user.name} />
+        <Header />
+        <main>
+          <section className="sub__header">
+            <Subheader
+              subheader="My Tasks"
+              button="Edit Tasks"
+              open={this.state.menuActive}
+              onClick={() => this.toggleMenu(!this.state.menuActive)}
+            />
+          </section>
+          <section className="list_box">
+            <ul>
+              {this.state.tasks.map((taskObj, index) => (
+                <Task task={taskObj} key={index} />
+              ))}
+            </ul>
+            <div className="new__task">
+              <Additem add="Create New Task" />
+            </div>
+          </section>
+        </main>
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Menu/Menu.styled.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const StyledMenu = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  background: rgba(240, 240, 240, 1);
+  transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
+  height: 100vh;
+  text-align: center;
+  padding: 2rem;
+  position: absolute;
+  top: 0;
+  right: 0;
+  transition: transform 0.3s ease-in-out;
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+      width: 100%;
+    }
+  a {
+    margin-top: 2rem;
+    outline: none;
+    font-size: 1rem;
+    text-transform: uppercase;
+    // padding: 1rem 0;
+    font-weight: bold;
+    letter-spacing: 0.5rem;
+    color: rgba(51, 51, 51, 1);
+    text-decoration: none;
+    transition: color 0.3s linear;
+    @media (max-width: ${({ theme }) => theme.mobile}) {
+      font-size: 1.5rem;
+      text-align: center;
+    }
+    &:hover {
+      color: rgba(224, 23, 131, 1);
+    }
+  }
+`;
